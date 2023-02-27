e175618f293d7968401f454d593c0439ed4c4c9e
Emilie Gonin <emilie.yuu@gmail.com>
2023-02-27 11:52:07 +0100

    clean files

--- Assets/AmbientSound.cs
+++ Assets/AmbientSound.cs
@@ -6,13 +6,6 @@ public class AmbientSound : MonoBehaviour
 {
     public AudioSource AmbientMusic;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (!AmbientMusic.isPlaying)
