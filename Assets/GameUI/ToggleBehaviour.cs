c57095dae861fcf1ffcc4d6e2c2c792af8971ff7
Ytaki2134 <tdurand@gaming.tech>
2023-03-02 12:41:43 +0100

    working gameMode selection

--- Assets/GameUI/ToggleBehaviour.cs
+++ Assets/GameUI/ToggleBehaviour.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class ToggleBehaviour : MonoBehaviour
 {
     public int ToggledValue { get; set; } = 1;
+    public int ToggledMode {get; set; } = 0;
     //[SerializeField] Spawner spawner;
+
+    //Difficulty Toggles
     public void SetEasy()
     {
         //spawner.SetDifficulty(1);
@@ -26,5 +29,24 @@ public class ToggleBehaviour : MonoBehaviour
         //Spawner.Instance.SetDifficulty(3);
         ToggledValue = 3;
         Difficulty.Instance.SetDifficulty(3);
+    }
+
+    //Mode Toggles
+    public void SetNormal()
+    {
+        ToggledMode = 0;
+        GameMode.Instance.SetGameMode(0);
+    }
+
+    public void SetLucky()
+    {
+        ToggledMode = 1;
+        GameMode.Instance.SetGameMode(1);
+    }
+
+    public void SetUnsafe()
+    {
+        ToggledMode = 2;
+        GameMode.Instance.SetGameMode(2);
     }
 }
